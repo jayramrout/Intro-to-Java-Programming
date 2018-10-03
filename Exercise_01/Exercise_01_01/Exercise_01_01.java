f36afda6ab41c185204c72791794dcc1076b3d95
jrout <jayram.rout@gmail.com>
2018-10-02 22:15:31 -0400

    Test

--- Exercise_01/Exercise_01_01/Exercise_01_01.java
+++ Exercise_01/Exercise_01_01/Exercise_01_01.java
@@ -5,6 +5,6 @@ public class Exercise_01_01 {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Java");
 		System.out.println("Welcome to Computer Science");
-		System.out.println("Programming is fun");
+		System.out.println("Programming is funn");
 	}
 }
